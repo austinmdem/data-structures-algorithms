39a5ddb0cc36b09ba8fc41b428ef3dd79c6b4aee
Austin Deming <austindeming@gmail.com>
2020-09-10 21:27:33 -0700

    first commit

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,37 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// 3rd Solution - Refactored
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
+
+// // 3rd Solution
+// function reverse(str) {
+//   return str.split('').reduce((reversed, character) => {
+//     return character + reversed;
+//   }, '');
+// }
+
+// 2nd Solution
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
+
+// 1st solution
+// function reverse(str) {
+//    return str.split('').reverse().join('');
+//  }
+
+module.exports = reverse;
